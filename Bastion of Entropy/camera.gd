51db87befe898a16e6f905ea8ec828c6f96d3141
Kosina Zoltán <kosinaz@gmail.com>
2024-03-25 16:14:03 +0100

    add movement tweens

--- Bastion of Entropy/camera.gd
+++ Bastion of Entropy/camera.gd
@@ -1,13 +1,22 @@
 extends Camera
 
-func _unhandled_input(event):
-	if event.is_action_pressed("ui_up"):
-		translation.x -= 2 * sin(rotation.y)
-		translation.z -= 2 * cos(rotation.y)
-	if event.is_action_pressed("ui_down"):
-		translation.x += 2 * sin(rotation.y)
-		translation.z += 2 * cos(rotation.y)
-	if event.is_action_pressed("ui_left"):
-		rotation_degrees.y += 90
-	if event.is_action_pressed("ui_right"):
-		rotation_degrees.y -= 90
+var tween = null
+
+var up = false
+var down = false
+
+func _process(_delta):
+	if tween != null and tween.is_running():
+		return
+	if Input.is_action_pressed("ui_left"):
+		tween = get_tree().create_tween()
+		tween.tween_property(self, "rotation_degrees:y", 90, 0.5).as_relative()
+	elif Input.is_action_pressed("ui_right"):
+		tween = get_tree().create_tween()
+		tween.tween_property(self, "rotation_degrees:y", -90, 0.5).as_relative()
+	elif Input.is_action_pressed("ui_up"):
+		tween = get_tree().create_tween()
+		tween.tween_property(self, "translation", -2 * Vector3(sin(rotation.y), 0, cos(rotation.y)), 0.5).as_relative()
+	elif Input.is_action_pressed("ui_down"):
+		tween = get_tree().create_tween()
+		tween.tween_property(self, "translation", 2 * Vector3(sin(rotation.y), 0, cos(rotation.y)), 0.5).as_relative()
