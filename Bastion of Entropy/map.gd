381d8f9f6d6f767eb8e7d2b5c6e9b79aaf4baca8
Kosina Zoltán <kosinaz@gmail.com>
2024-03-27 15:45:30 +0100

    refactor player and add all stairs

--- Bastion of Entropy/map.gd
+++ Bastion of Entropy/map.gd
@@ -2,8 +2,6 @@ extends Spatial
 
 var block_scene = preload("res://block.tscn")
 var stairs_scene = preload("res://stairs.tscn")
-var tween = null
-var moving = false
 var blocks = {}
 
 func _ready():
@@ -15,6 +13,34 @@ func _ready():
 	blocks[Vector3(0, 0, 1)] = null
 	blocks[Vector3(0, 0, 2)] = null
 	blocks[Vector3(0, 0, 3)] = null
+	blocks[Vector3(-1, 0, 3)] = null
+	blocks[Vector3(-2, 0, 3)] = null
+	blocks[Vector3(-2, 0, 4)] = null
+	blocks[Vector3(-2, 0, 5)] = null
+	blocks[Vector3(-1, 0, 5)] = null
+	blocks[Vector3(-1, 0, 6)] = null
+	blocks[Vector3(-1, 0, 7)] = null
+	blocks[Vector3(-1, 0, 8)] = null
+	blocks[Vector3(-1, 1, 5)] = null
+	blocks[Vector3(-1, 1, 6)] = null
+	blocks[Vector3(-1, 1, 7)] = null
+	blocks[Vector3(-1, 1, 8)] = null
+	blocks[Vector3(-2, 0, 5)] = null
+	blocks[Vector3(-2, 0, 6)] = null
+	blocks[Vector3(-2, 0, 7)] = null
+	blocks[Vector3(-2, 0, 8)] = null
+	blocks[Vector3(-2, 1, 5)] = null
+	blocks[Vector3(-2, 1, 6)] = null
+	blocks[Vector3(-2, 1, 7)] = null
+	blocks[Vector3(-2, 1, 8)] = null
+	blocks[Vector3(-3, 0, 5)] = null
+	blocks[Vector3(-3, 0, 6)] = null
+	blocks[Vector3(-3, 0, 7)] = null
+	blocks[Vector3(-3, 0, 8)] = null
+	blocks[Vector3(-3, 1, 5)] = null
+	blocks[Vector3(-3, 1, 6)] = null
+	blocks[Vector3(-3, 1, 7)] = null
+	blocks[Vector3(-3, 1, 8)] = null
 	blocks[Vector3(0, 0, 4)] = "stairs180"
 	blocks[Vector3(1, 0, 0)] = null
 	blocks[Vector3(2, 0, 0)] = null
@@ -24,6 +50,14 @@ func _ready():
 	blocks[Vector3(-1, 0, 0)] = null
 	blocks[Vector3(0, 1, 4)] = null
 	blocks[Vector3(0, 1, 5)] = null
+	blocks[Vector3(1, 1, 5)] = null
+	blocks[Vector3(1, 0, 5)] = "stairs90"
+	blocks[Vector3(2, 0, 5)] = null
+	blocks[Vector3(2, 0, 4)] = "stairs0"
+	blocks[Vector3(2, 1, 4)] = null
+	blocks[Vector3(2, 1, 3)] = null
+	blocks[Vector3(1, 1, 3)] = null
+	blocks[Vector3(1, 0, 3)] = "stairs270"
 	blocks[Vector3(0, 1, 6)] = null
 	blocks[Vector3(0, 1, 7)] = null
 	blocks[Vector3(0, 1, 8)] = null
@@ -39,65 +73,34 @@ func _ready():
 		elif blocks[block].begins_with("stairs"):
 			var stairs_instance = stairs_scene.instance()
 			stairs_instance.rotation_degrees.y = int(blocks[block].right(6))
-			stairs_instance.translation = Vector3(0, 0, 4)
+			stairs_instance.translation = block
 			$Blocks.add_child(stairs_instance)
 
 func _process(_delta):
-	if moving:
+	if $Player.moving:
 		return
 	if Input.is_action_pressed("ui_left"):
-		moving = true
-		tween = get_tree().create_tween()
-		tween.tween_property($CameraNode, "rotation_degrees:y", round($CameraNode.rotation_degrees.y + 90), 0.5)
-		tween.tween_callback(self, "_stop")
+		$Player.rotate_left()
 	elif Input.is_action_pressed("ui_right"):
-		moving = true
-		tween = get_tree().create_tween()
-		tween.tween_property($CameraNode, "rotation_degrees:y", round($CameraNode.rotation_degrees.y - 90), 0.5)
-		tween.tween_callback(self, "_stop")
+		$Player.rotate_right()
 	elif Input.is_action_pressed("ui_up"):
-		var next_block: Vector3 = $CameraNode.translation - 1 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y)).round()
-		print(next_block, $CameraNode.rotation_degrees.y)
-		if blocks[next_block + Vector3(0, -1, 0)] == null:
+		print($Player.get_map_rotation_plus(180))
+		if blocks[$Player.get_forward_down_block()] == null:
+			print("gap ahead: ", $Player.get_forward_down_block())
 			return
-		if blocks[next_block + Vector3(0, -1, 0)] == "stairs" + str(int(round($CameraNode.rotation_degrees.y + 180)) % 360):
-			moving = true
-			var step1 = $CameraNode.translation - 0.5 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y))
-			var step2 = $CameraNode.translation - 1.5 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y)) + Vector3(0, -1, 0)
-			var step3 = $CameraNode.translation - 2 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y)).round() + Vector3(0, -1, 0)
-			tween = get_tree().create_tween()
-			tween.tween_property($CameraNode, "translation", step1, 0.25)
-			tween.tween_property($CameraNode, "translation", step2, 0.5)
-			tween.tween_property($CameraNode, "translation", step3, 0.25)
-			tween.tween_callback(self, "_stop")
-		if blocks[next_block] == null:
-			moving = true
-			tween = get_tree().create_tween()
-			tween.tween_property($CameraNode, "translation", next_block, 0.5)
-			tween.tween_callback(self, "_stop")
-		elif blocks[next_block] == "stairs" + str(int(round($CameraNode.rotation_degrees.y)) % 360):
-			moving = true
-			var step1 = $CameraNode.translation - 0.5 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y))
-			var step2 = $CameraNode.translation - 1.5 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y)) + Vector3(0, 1, 0)
-			var step3 = $CameraNode.translation - 2 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y)).round() + Vector3(0, 1, 0)
-			tween = get_tree().create_tween()
-			tween.tween_property($CameraNode, "translation", step1, 0.25)
-			tween.tween_property($CameraNode, "translation", step2, 0.5)
-			tween.tween_property($CameraNode, "translation", step3, 0.25)
-			tween.tween_callback(self, "_stop")
+		elif blocks[$Player.get_forward_down_block()] == "stairs" + str($Player.get_map_rotation_plus(180)):
+			$Player.move_forward_down_forward()
+		elif blocks[$Player.get_forward_block()] == null:
+			$Player.move_forward()
+		elif blocks[$Player.get_forward_block()] == "stairs" + str($Player.get_map_rotation_plus(0)):
+			$Player.move_forward_up_forward()
 	elif Input.is_action_pressed("ui_down"):
-		var next_block = $CameraNode.translation + 1 * Vector3(sin($CameraNode.rotation.y), 0, cos($CameraNode.rotation.y)).round()
-		if blocks[next_block] == "block":
+		if blocks[$Player.get_backward_down_block()] == null:
+			print("gap behind: ", $Player.get_backward_down_block())
 			return
-		tween = get_tree().create_tween()
-		tween.tween_property($CameraNode, "translation", next_block, 0.5)
-	elif Input.is_action_pressed("ui_page_up"):
-		tween = get_tree().create_tween()
-		tween.tween_property($CameraNode, "translation", Vector3(0, 0.5, 0), 0.5).as_relative()
-	elif Input.is_action_pressed("ui_page_down"):
-		tween = get_tree().create_tween()
-		tween.tween_property($CameraNode, "translation", Vector3(0, -0.5, 0), 0.5).as_relative()
-
-func _stop():
-	moving = false
-	print("stopped at ", $CameraNode.translation)
+		elif blocks[$Player.get_backward_down_block()] == "stairs" + str($Player.get_map_rotation_plus(0)):
+			$Player.move_backward_down_backward()
+		elif blocks[$Player.get_backward_block()] == null:
+			$Player.move_backward()
+		elif blocks[$Player.get_backward_block()] == "stairs" + str($Player.get_map_rotation_plus(180)):
+			$Player.move_backward_up_backward()
---
+++ Bastion of Entropy/player.gd
@@ -0,0 +1,111 @@
+extends Spatial
+
+var map_rotation = 0
+var map_translation = Vector3(0, 0, 0)
+var moving = false
+
+func rotate_left():
+	moving = true
+	map_rotation += 90
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "rotation_degrees:y", map_rotation, 0.5)
+	tween.tween_callback(self, "_stop")
+
+func rotate_right():
+	moving = true
+	map_rotation -= 90
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "rotation_degrees:y", map_rotation, 0.5)
+	tween.tween_callback(self, "_stop")
+
+func get_map_rotation_plus(degrees):
+	return (map_rotation + degrees) % 360
+
+func get_forward_block():
+	return map_translation - 1 * Vector3(sin(deg2rad(map_rotation)), 0, cos(deg2rad(map_rotation))).round()
+
+func get_forward_down_block():
+	return get_forward_block() + Vector3(0, -1, 0)
+
+func get_forward_down_forward_block():
+	return get_forward_down_block() - 1 * Vector3(sin(deg2rad(map_rotation)), 0, cos(deg2rad(map_rotation))).round()
+	
+func get_forward_up_forward_block():
+	return map_translation - 2 * Vector3(sin(deg2rad(map_rotation)), 0, cos(deg2rad(map_rotation))).round() + Vector3(0, 1, 0)
+
+func get_backward_block():
+	return map_translation + 1 * Vector3(sin(deg2rad(map_rotation)), 0, cos(deg2rad(map_rotation))).round()
+
+func get_backward_down_block():
+	return get_backward_block() + Vector3(0, -1, 0)
+
+func get_backward_down_backward_block():
+	return get_backward_down_block() + 1 * Vector3(sin(deg2rad(map_rotation)), 0, cos(deg2rad(map_rotation))).round()
+	
+func get_backward_up_backward_block():
+	return map_translation + 2 * Vector3(sin(deg2rad(map_rotation)), 0, cos(deg2rad(map_rotation))).round() + Vector3(0, 1, 0)
+
+func move_forward_down_forward():
+	moving = true
+	map_translation = get_forward_down_forward_block()
+	var tween_x = get_tree().create_tween()
+	tween_x.tween_property(self, "translation:x", map_translation.x, 1)
+	tween_x.tween_callback(self, "_stop")
+	var tween_y = get_tree().create_tween()
+	tween_y.tween_interval(0.3)
+	tween_y.tween_property(self, "translation:y", map_translation.y, 0.5)
+	var tween_z = get_tree().create_tween()
+	tween_z.tween_property(self, "translation:z", map_translation.z, 1)
+	
+func move_forward():
+	moving = true
+	map_translation = get_forward_block()
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "translation", map_translation, 0.5)
+	tween.tween_callback(self, "_stop")
+
+func move_forward_up_forward():
+	moving = true
+	map_translation = get_forward_up_forward_block()
+	var tween_x = get_tree().create_tween()
+	tween_x.tween_property(self, "translation:x", map_translation.x, 1)
+	tween_x.tween_callback(self, "_stop")
+	var tween_y = get_tree().create_tween()
+	tween_y.tween_interval(0.3)
+	tween_y.tween_property(self, "translation:y", map_translation.y, 0.5)
+	var tween_z = get_tree().create_tween()
+	tween_z.tween_property(self, "translation:z", map_translation.z, 1)
+
+func move_backward_down_backward():
+	moving = true
+	map_translation = get_backward_down_backward_block()
+	var tween_x = get_tree().create_tween()
+	tween_x.tween_property(self, "translation:x", map_translation.x, 1)
+	tween_x.tween_callback(self, "_stop")
+	var tween_y = get_tree().create_tween()
+	tween_y.tween_interval(0.2)
+	tween_y.tween_property(self, "translation:y", map_translation.y, 0.5)
+	var tween_z = get_tree().create_tween()
+	tween_z.tween_property(self, "translation:z", map_translation.z, 1)
+
+func move_backward():
+	moving = true
+	map_translation = get_backward_block()
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "translation", map_translation, 0.5)
+	tween.tween_callback(self, "_stop")
+
+func move_backward_up_backward():
+	moving = true
+	map_translation = get_backward_up_backward_block()
+	var tween_x = get_tree().create_tween()
+	tween_x.tween_property(self, "translation:x", map_translation.x, 1)
+	tween_x.tween_callback(self, "_stop")
+	var tween_y = get_tree().create_tween()
+	tween_y.tween_interval(0.2)
+	tween_y.tween_property(self, "translation:y", map_translation.y, 0.5)
+	var tween_z = get_tree().create_tween()
+	tween_z.tween_property(self, "translation:z", map_translation.z, 1)
+
+func _stop():
+	moving = false
