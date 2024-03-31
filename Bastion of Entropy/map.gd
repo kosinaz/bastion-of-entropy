c5e1e5e3c13008631522a11145ceb10a41ee7f33
Kosina Zoltán <kosinaz@gmail.com>
2024-03-31 09:15:12 +0200

    disable shadows

--- Bastion of Entropy/map.gd
+++ Bastion of Entropy/map.gd
@@ -17,19 +17,16 @@ var left_is_down = false
 var down_is_down = false
 var right_is_down = false
 var portal = null
+var map_data = MapData.new()
 
 func _ready():
 	for x in range(-20, 20):
 		for z in range(-20, 20):
 			for y in range(-20, 20):
 				blocks[Vector3(x, y, z)] = null
-	
-	var file = File.new()
-	file.open("map.ply", File.READ)
-	for _i in range(11):
-		file.get_csv_line()
-	while file.get_position() < file.get_len():
-		var content = file.get_csv_line(" ")
+	var map_data_rows = map_data.map.split("\n")
+	for map_data_row in map_data_rows:
+		var content = map_data_row.split(" ")
 		var block_name = "block"
 		if content[3] + content[4] + content[5] == "238238238":
 			$Player.translation = Vector3(int(content[0]), int(content[2]), int(content[1]))
@@ -46,7 +43,6 @@ func _ready():
 		elif content[3] + content[4] + content[5] == "686868":
 			block_name = "moving_block"
 		blocks[Vector3(int(content[0]), int(content[2]), int(content[1]))] = block_name
-	file.close()
 	
 	for block in blocks:
 		if blocks[block] == null:
@@ -220,4 +216,5 @@ func flip_time():
 		$"%Flip".disabled = true
 
 func _on_restart_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().reload_current_scene()
